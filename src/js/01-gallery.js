b0ac76c0641efadd4aa375441188958ff1d05bd7
Ievgen Shakin <shakinzhenya@gmail.com>
2021-12-19 13:13:03 +0200

    дз№8

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,26 @@
+import SimpleLightbox from 'simplelightbox';
+import 'simplelightbox/dist/simple-lightbox.min.css';
 // Add imports above this line
 import { galleryItems } from './gallery-items';
 // Change code below this line
 
 console.log(galleryItems);
+
+const refs = {
+  gallery: document.querySelector(".gallery"),
+
+};
+
+const galleryItemsString = galleryItems
+  .map(
+    (galleryItem) =>
+      `<li class="gallery__item"><a class="gallery__link" href="${galleryItem.original}">
+      <img class="gallery__image" src="${galleryItem.preview}" alt="${galleryItem.description}" />
+      </a></li>`
+  )
+  .join("");
+
+refs.gallery.insertAdjacentHTML("beforeend", galleryItemsString);
+
+
+new SimpleLightbox('.gallery a', { captionsData: 'alt', captionDelay: 250 });
--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,45 @@
+import throttle from "lodash.throttle";
 
+const refs ={
+    form: document.querySelector('.feedback-form'),
+    email: document.querySelector('.feedback-form input'),
+    textarea: document.querySelector(".feedback-form textarea")
+}
+
+refs.form.addEventListener('input', throttle(onInputChanges, 500 ));
+
+const formData = {
+    email: '',
+    message: ''
+}
+
+function onInputChanges(e){
+    formData[e.target.name] = e.target.value;
+    localStorage.setItem('feedback-form-state', JSON.stringify(formData))
+};
+
+refs.form.addEventListener('submit', onFormSubmit);
+
+function onFormSubmit(e){
+    e.preventDefault();
+    e.currentTarget.reset()
+    localStorage.removeItem('feedback-form-state')
+    console.log(formData);
+}
+
+function onFormChange(){
+    const saveMsg = localStorage.getItem('feedback-form-state');
+    const parseMsg = JSON.parse(saveMsg)
+    try {
+      if (parseMsg) {
+          refs.email.value =  parseMsg.email;
+          refs.textarea.value =  parseMsg.message; 
+      } 
+    }
+    catch (error) {
+      console.log(error.name); 
+      console.log(error.message);
+    }  
+}
+
+onFormChange()
