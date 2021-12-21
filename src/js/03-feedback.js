cc637f42326d77dac4a25993f82c36af1b46b77d
Ievgen Shakin <shakinzhenya@gmail.com>
2021-12-21 23:51:10 +0200

    upd 03

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -3,14 +3,14 @@ import throttle from "lodash.throttle";
 const STORAGE_KEY = 'feedback-form-state';
 
 const refs ={
-    form: document.querySelector('.feedback-form'),
-    email: document.querySelector('.feedback-form input'),
-    textarea: document.querySelector(".feedback-form textarea")
+  form: document.querySelector('.feedback-form'),
+  email: document.querySelector('.feedback-form input'),
+  textarea: document.querySelector(".feedback-form textarea")
 }
 
 let formData = {
-    email: '',
-    message: ''
+  email: '',
+  message: ''
 }
 
 const savedMsg = localStorage.getItem(STORAGE_KEY);
@@ -20,19 +20,21 @@ refs.form.addEventListener('input', throttle(onInputChanges, 500));
 refs.form.addEventListener('submit', onFormSubmit);
 
 function onInputChanges(e){
-    formData[e.target.name] = e.target.value;
-    localStorage.setItem(STORAGE_KEY, JSON.stringify(formData))
+  formData[e.target.name] = e.target.value;
+  localStorage.setItem(STORAGE_KEY, JSON.stringify(formData))
 };
 
 function onFormSubmit(e){
   e.preventDefault();
   e.currentTarget.reset();
   localStorage.removeItem(STORAGE_KEY);
+ 
   if (parsedMsg) {
     console.log(parsedMsg);
   }
   else { 
     console.log(formData);
+    {formData.email ='', formData.message =''}
   }  
 }
 
@@ -40,7 +42,7 @@ onFormChange()
 
 function onFormChange(){
   if (parsedMsg) {
-      refs.email.value = parsedMsg.email;
-      refs.textarea.value = parsedMsg.message; 
+    refs.email.value = parsedMsg.email;
+    refs.textarea.value = parsedMsg.message; 
   } 
 }
